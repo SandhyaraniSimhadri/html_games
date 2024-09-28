31b841a73f7ab542d3f5a14a831e267706f533c8
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2024-09-28 22:19:37 +0530

    issues

--- 90/scripts/modernjscheck.js
+++ 90/scripts/modernjscheck.js
@@ -1,1 +1,10 @@
-'use strict';(function(){let pass=true;if((null??2)!==2)pass=false;if(pass)window["C3_ModernJSSupport_OK"]=true})();
+'use strict';
+(function() {
+  let pass = true;
+
+  // Since (null ?? 2) will always return 2, this condition will never be true
+  // If this is intended to check for modern JS support, no need for the comparison
+  pass = false;  // Directly set the pass to false since the condition is redundant
+
+  if (pass) window["C3_ModernJSSupport_OK"] = true;
+})();
